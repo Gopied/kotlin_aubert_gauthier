103221c3f100ea8a9d623e06d090678904d4fbcf
Gauthier <gauthier.aubert@devinci.fr>
2019-05-28 23:36:22 +0200

    first commit

---
+++ app/src/main/java/aubert/gauthier/myandroidapplication/OtherActivity.kt
@@ -0,0 +1,14 @@
+package aubert.gauthier.myandroidapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OtherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_other)
+    }
+
+}
